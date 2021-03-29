8cb9e0a0a44fd57ab45a5b8a6c4027f4453ea1c6
Sai Giridhar Rao Allada <sai.giridhar.rao.allada@ibm.com>
2021-03-30 04:35:09 +0530

    jwt added

--- src/main/java/com/stackroute/reccomendationservice/ReccomendationserviceApplication.java
+++ src/main/java/com/stackroute/reccomendationservice/ReccomendationserviceApplication.java
@@ -1,10 +1,25 @@
 package com.stackroute.reccomendationservice;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.stackroute.reccomendationservice.jwtfilter.*;
 
 @SpringBootApplication
 public class ReccomendationserviceApplication {
+	
+	 @Bean
+	    public FilterRegistrationBean jwtFilter() {
+		  FilterRegistrationBean<Filter> filter = new FilterRegistrationBean <>();
+		  filter.addUrlPatterns("/api/v1/gifs/*");
+		  filter.setFilter(new JwtFilter());
+		  return filter;
+
+	    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReccomendationserviceApplication.class, args);
